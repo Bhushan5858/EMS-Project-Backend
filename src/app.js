a7f78d0c65f83aececad7ad5fe6fed2ebacafe93
Bhushan5858 <patilpavan5858@gmail.com>
2026-04-01 12:10:00 +0530

    frontend url chnaged

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: 'https://ems-project-frontend-dmq9.vercel.app',
         credentials: true
     }
 ));
